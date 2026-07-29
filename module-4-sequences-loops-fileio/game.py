78a96865056da7217372fb0249bdf95dac54e7ba
jasmineilorah1 <jasmine.ilorah.1@gmail.com>
2026-07-28 22:55:41 -0400

    Done!

--- module-4-sequences-loops-fileio/game.py
+++ module-4-sequences-loops-fileio/game.py
@@ -18,9 +18,37 @@ def show_previous_ending():
 
 
 def main():
-    # TODO: show previous ending, then run the game with a while loop over SCENES
-    pass
+    print("Solo Leveling")
 
+    name = input("What is players name?")
+    print( "welcome", name, "to Solo Leveling")
+
+    scene= input("Write scene of your choice :")
+    print(scene)
+    print("And you notice youre in a new dimension and you have powers")
+
+    power = input("Do you want Super strength or Invisibility?").strip().lower()
+    if power == "super strength":
+       print("You have now gained Super strength")
+    elif power == "invisibility":
+       print("You have now gained Invisibility")
+    else:
+       print("You have not chosen a valid power, you remain normal.")
+    scene =[ "You decide to go explore the new universe",
+          "And you notice this new world goes against everything you know", 
+          "In this new world mythical creatures exist",
+          "You watch two fairies zoom past you"]
+    scene_number = 0
+    while scene_number < len(scene) :      
+        print(scene[scene_number] )
+        scene_number= scene_number + 1
+    ending = "You gasped, eyes wide!"
+    with open("ending.txt", "w") as file: 
+        file.write(ending) 
+
+    with open("ending.txt", "r") as file:
+        saved_ending = file.read() 
+        print("ending:", saved_ending)
 
 if __name__ == "__main__":
     main()
